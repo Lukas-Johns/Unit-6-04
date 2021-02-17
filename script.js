bca49531bd352ccfa2ce958eedbc9a27d9707263
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-17 17:55:29 +0000

    space

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let harold = 0
 let jesse = 0
 let kyle = 0
 let matt = 0
-/* Above are the different variables of the program. harold is representing a, jesse is representing b, kyle is representing h, and matt is representing the area. Below is a button that connects the button to the main function*/
+/* Above are the different variables of the program. harold is representing a, jesse is representing b, kyle is representing h, and matt is representing the area. Below is a button that connects the button to the main function */
 document.getElementById('press').addEventListener('click', myInput)
 /* Below is a function that first interprets the values for each variable in lines 8-14. parseInts help the variables function as numbers in an equation. Then it uses the variables in a formula to find an area for the trapezoid in line 15. Finally line 16 sends out an alert to sho the user the value of area */
 function myInput () {

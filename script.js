e7a6487f9599f8008ef8b26099ef33d97f988350
LukasJohns <lukas.johns@ocsbstudent.ca>
2021-02-17 17:18:40 +0000

    Linter Fixed maybe?

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ let kyle = 0
 let matt = 0
 document.getElementById('press').addEventListener('click', myInput)
 function myInput () {
-  harold = document.getElementById('valueA').value
+  harold = document.getElementById('value-a').value
   harold = parseInt(harold)
-  jesse = document.getElementById('valueB').value
+  jesse = document.getElementById('value-b').value
   jesse = parseInt(jesse)
-  kyle = document.getElementById('valueC').value
+  kyle = document.getElementById('value-c').value
   kyle = parseInt(kyle)
   matt = (((harold + jesse) / 2) * kyle)
   alert(matt)
